cd7c1c664099d19c49c36366ab68aea593437354
Yuri Viana Fernandes <y.fernandes@vp6.com.br>
2024-09-01 15:36:06 -0300

    Criado os comandos para preencher os registros nas tabelas e ajustado alguns campos nas tabelas.

--- src/cadastros/models/loja.py
+++ src/cadastros/models/loja.py
@@ -1,9 +1,23 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 
 class Loja(models.Model):
+    telefone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="O número de telefone deve estar no formato '+999999999'. Até 15 dígitos permitidos."
+    )
     nome = models.CharField(max_length=255, unique=True)
     codigo = models.CharField(max_length=15, unique=True)
+    bairro = models.CharField(max_length=255)
+    cep = models.CharField(max_length=8)
+    cidade = models.CharField(max_length=255)
+    cnpj = models.CharField(max_length=15, unique=True)
+    email = models.CharField(max_length=300)
+    rua = models.CharField(max_length=255)
+    telefone = models.CharField(
+        validators=[telefone_regex], max_length=17, blank=True)
+    uf = models.CharField(max_length=2)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now_add=True, null=True)
 
