c27d5b365d673f636dc22a8deca77ff855f46fe7
Yuri Viana Fernandes <yuri.viana.fernandes@gmail.com>
2024-10-28 20:31:14 -0300

    2024.10.28 - Ajustes na estrutura do projeto. Cadastros: Criado o comando para preencher a tabela de clientes. Receita: Ajustado as tabelas venda e consolidação incluindo o cliente. Ajustado as tasks de load_consolidado e load_consolidado_pandas para incluir o cliente.

--- src/receita/tasks/load_consolidado.py
+++ src/receita/tasks/load_consolidado.py
@@ -15,11 +15,11 @@ class LoadConsolidacao(Pipeline):
 
     def __init__(self, **kwargs) -> None:
         super().__init__()
-        self.data_range = kwargs.get('data_range')
-        self.vendedor_list = kwargs.get('vendedor_list')
-        self.produto_list = kwargs.get('produto_list')
-        self.loja_list = kwargs.get('loja_list')
-        self.equipe_venda_list = kwargs.get('equipe_venda_list')
+        self.data_range = kwargs.get("data_range")
+        self.vendedor_list = kwargs.get("vendedor_list")
+        self.produto_list = kwargs.get("produto_list")
+        self.loja_list = kwargs.get("loja_list")
+        self.equipe_venda_list = kwargs.get("equipe_venda_list")
 
     def get_imposto_queryset(self) -> QuerySet:
         """Retorna o queryset de [Imposto]"""
@@ -31,141 +31,139 @@ class LoadConsolidacao(Pipeline):
 
     def get_venda_queryset(self) -> QuerySet:
         """Retorna o queryset de [Venda]"""
-        return (
-            Venda
-            .objects
-            .filter(**self._filtro)
-            .annotate_with_receita_bruta())
+        return Venda.objects.filter(
+            **self._filtro
+        ).annotate_with_receita_bruta()
 
     @cached_property
     def _filtro(self) -> dict:
         filtro = {}
         if not None in self.data_range:
-            filtro['data__range'] = self.data_range
+            filtro["data__range"] = self.data_range
         if self.vendedor_list:
-            filtro['vendedor__in'] = self.vendedor_list
+            filtro["vendedor__in"] = self.vendedor_list
         if self.loja_list:
-            filtro['vendedor__equipe_venda__loja__in'] = self.loja_list
+            filtro["vendedor__equipe_venda__loja__in"] = self.loja_list
         if self.equipe_venda_list:
-            filtro['vendedor__equipe_venda__in'] = self.equipe_venda_list
+            filtro["vendedor__equipe_venda__in"] = self.equipe_venda_list
         if self.produto_list:
-            filtro['produto__in'] = self.produto_list
+            filtro["produto__in"] = self.produto_list
         return filtro
 
     def run(self) -> dict:
         start_polars_transform = timezone.now()
         self.extract_transform_dataset()
         finish_polars_transform = timezone.now()
-        self.log['duration_transform'] = (
-            finish_polars_transform -
-            start_polars_transform).total_seconds()
+        self.log["duration_transform"] = (
+            finish_polars_transform - start_polars_transform
+        ).total_seconds()
         self.load()
         return self.log
 
     def extract_transform_dataset(self) -> None:
         self.dataset = (
-            self._venda_dataset
-            .join(
-                self._imposto_dataset,
-                how='left',
-                on=['loja_id'])
-            .with_columns(
-                (
-                    pl.col('valor_receita_bruta') *
-                    (pl.col('percent_imposto')/100)
-                )
-                .alias('valor_impostos')
+            self._venda_dataset.join(
+                self._imposto_dataset, how="left", on=["loja_id"]
             )
             .with_columns(
                 (
-                    (
-                        pl.col('valor_receita_bruta') -
-                        pl.col('valor_impostos')) *
-                    (pl.col('percent_comissao')/100)
-                )
-                .alias('valor_comissao')
+                    pl.col("valor_receita_bruta")
+                    * (pl.col("percent_imposto") / 100)
+                ).alias("valor_impostos")
             )
             .with_columns(
                 (
-                    pl.col('valor_receita_bruta') -
-                    pl.col('valor_comissao') -
-                    pl.col('valor_impostos') -
-                    pl.col('valor_custo')
-                )
-                .alias('valor_receita_liquida')
+                    (pl.col("valor_receita_bruta") - pl.col("valor_impostos"))
+                    * (pl.col("percent_comissao") / 100)
+                ).alias("valor_comissao")
+            )
+            .with_columns(
+                (
+                    pl.col("valor_receita_bruta")
+                    - pl.col("valor_comissao")
+                    - pl.col("valor_impostos")
+                    - pl.col("valor_custo")
+                ).alias("valor_receita_liquida")
             )
             .pipe(self._reshape_data)
             .with_columns(
-                pl.col('receita_tipo')
+                pl.col("receita_tipo")
                 .replace(
                     {
-                        f'valor_{obj.codigo}': obj.id
+                        f"valor_{obj.codigo}": obj.id
                         for obj in self.get_tipo_queryset()
-                    }, default=None
+                    },
+                    default=None,
                 )
-                .alias('tipo_id')
+                .alias("tipo_id")
             )
-            .group_by(['data', 'produto_id', 'vendedor_id', 'tipo_id'])
-            .agg(pl.col('valor').sum().alias('valor'))
+            .group_by(
+                ["data", "produto_id", "vendedor_id", "tipo_id", "cliente_id"]
+            )
+            .agg(pl.col("valor").sum().alias("valor"))
         )
 
     @property
     def _venda_dataset(self) -> pl.DataFrame:
         """Retorna um dataset com os dados de [Venda]"""
         schema = {
-            'data': {'rename': 'data', 'type': pl.Date},
-            'produto_id': {'rename': 'produto_id', 'type': pl.Int64},
-            'vendedor_id': {'rename': 'vendedor_id', 'type': pl.Int64},
-            'vendedor__equipe_venda__loja_id': {'rename': 'loja_id', 'type': pl.Int64},
-            'valor_receita_bruta': {'rename': 'valor_receita_bruta', 'type': pl.Float64},
-            'vendedor__equipe_venda__percent_comissao': {'rename': 'percent_comissao', 'type': pl.Float64},
+            "data": {"rename": "data", "type": pl.Date},
+            "cliente_id": {"rename": "cliente_id", "type": pl.Int64},
+            "produto_id": {"rename": "produto_id", "type": pl.Int64},
+            "vendedor_id": {"rename": "vendedor_id", "type": pl.Int64},
+            "vendedor__equipe_venda__loja_id": {
+                "rename": "loja_id",
+                "type": pl.Int64,
+            },
+            "valor_receita_bruta": {
+                "rename": "valor_receita_bruta",
+                "type": pl.Float64,
+            },
+            "vendedor__equipe_venda__percent_comissao": {
+                "rename": "percent_comissao",
+                "type": pl.Float64,
+            },
         }
         qs = (
             self.get_venda_queryset()
             .values(*schema.keys())
-            .annotate(valor_custo=Sum('produto__custo'))
+            .annotate(valor_custo=Sum("produto__custo"))
         )
-        return (
-            pl.DataFrame(
-                data=list(qs),
-                schema=dict(
-                    **{k: v.get('type') for k, v in schema.items()},
-                    valor_custo=pl.Float64
-                )
-            )
-            .rename({k: v['rename'] for k, v in schema.items()}))
+        return pl.DataFrame(
+            data=list(qs),
+            schema=dict(
+                **{k: v.get("type") for k, v in schema.items()},
+                valor_custo=pl.Float64,
+            ),
+        ).rename({k: v["rename"] for k, v in schema.items()})
 
     @property
     def _imposto_dataset(self) -> pl.DataFrame:
         """Retorna um dataset com os dados de [Imposto]"""
         qs = (
             self.get_imposto_queryset()
-            .values('loja_id')
-            .annotate(percent_imposto=Sum('percent'))
+            .values("loja_id")
+            .annotate(percent_imposto=Sum("percent"))
         )
-        return (
-            pl.DataFrame(
-                data=list(qs),
-                schema=dict(
-                    loja_id=pl.Int64,
-                    percent_imposto=pl.Float64
-                )
-            )
+        return pl.DataFrame(
+            data=list(qs),
+            schema=dict(loja_id=pl.Int64, percent_imposto=pl.Float64),
         )
 
     def _reshape_data(self, df: pl.DataFrame) -> pl.DataFrame:
         """Transforma todas as colunas de valores em duas, uma de tipo e outra de valor."""
-        return (
-            df
-            .melt(
-                id_vars=[
-                    'data', 'produto_id',
-                    'vendedor_id', 'loja_id'
-                ],
-                value_vars=[
-                    col for col in df.columns if col.startswith('valor_')],
-                value_name='valor',
-                variable_name='receita_tipo'))
+        return df.melt(
+            id_vars=[
+                "data",
+                "cliente_id",
+                "produto_id",
+                "vendedor_id",
+                "loja_id",
+            ],
+            value_vars=[col for col in df.columns if col.startswith("valor_")],
+            value_name="valor",
+            variable_name="receita_tipo",
+        )
 
     @transaction.atomic
     def load(self) -> None:
@@ -173,37 +171,25 @@ class LoadConsolidacao(Pipeline):
         self._save()
 
     def _delete(self):
-        n_deleted, __ = (
-            Consolidacao
-            .objects
-            .filter(**self._filtro)
-            .delete()
-        )
-        self.log['n_deleted'] = n_deleted
+        n_deleted, __ = Consolidacao.objects.filter(**self._filtro).delete()
+        self.log["n_deleted"] = n_deleted
 
     def _save(self):
         if self.dataset.is_empty():
             return
 
-        objs = [
-            Consolidacao(**vals)
-            for vals in
-            self.dataset.to_dicts()]
-        bulk = (
-            Consolidacao
-            .objects
-            .bulk_create(
-                objs=objs,
-                batch_size=1000))
-        self.log['n_inserted'] = len(bulk)
+        objs = [Consolidacao(**vals) for vals in self.dataset.to_dicts()]
+        bulk = Consolidacao.objects.bulk_create(objs=objs, batch_size=1000)
+        self.log["n_inserted"] = len(bulk)
 
 
 @shared_task(
-    name='receita.load_consolidacao',
+    name="receita.load_consolidacao",
     bind=True,
     autoretry_for=(Exception,),
     retry_backoff=5,
-    retry_kwargs={'max_retries': 3})
+    retry_kwargs={"max_retries": 3},
+)
 def load_consolidacao_async(self, filtros: dict) -> dict:
     with LoadConsolidacao(**filtros) as task:
         log = task.run()
--- src/receita/tasks/load_consolidado_pandas.py
+++ src/receita/tasks/load_consolidado_pandas.py
@@ -15,11 +15,11 @@ class LoadConsolidacaoPandas(Pipeline):
 
     def __init__(self, **kwargs) -> None:
         super().__init__()
-        self.data_range = kwargs.get('data_range')
-        self.vendedor_list = kwargs.get('vendedor_list')
-        self.produto_list = kwargs.get('produto_list')
-        self.loja_list = kwargs.get('loja_list')
-        self.equipe_venda_list = kwargs.get('equipe_venda_list')
+        self.data_range = kwargs.get("data_range")
+        self.vendedor_list = kwargs.get("vendedor_list")
+        self.produto_list = kwargs.get("produto_list")
+        self.loja_list = kwargs.get("loja_list")
+        self.equipe_venda_list = kwargs.get("equipe_venda_list")
 
     def get_imposto_queryset(self) -> QuerySet:
         """Retorna o queryset de [Imposto]"""
@@ -31,117 +31,122 @@ class LoadConsolidacaoPandas(Pipeline):
 
     def get_venda_queryset(self) -> QuerySet:
         """Retorna o queryset de [Venda]"""
-        return (
-            Venda
-            .objects
-            .filter(**self._filtro)
-            .annotate_with_receita_bruta())
+        return Venda.objects.filter(
+            **self._filtro
+        ).annotate_with_receita_bruta()
 
     @cached_property
     def _filtro(self) -> dict:
         filtro = {}
         if not None in self.data_range:
-            filtro['data__range'] = self.data_range
+            filtro["data__range"] = self.data_range
         if self.vendedor_list:
-            filtro['vendedor__in'] = self.vendedor_list
+            filtro["vendedor__in"] = self.vendedor_list
         if self.loja_list:
-            filtro['vendedor__equipe_venda__loja__in'] = self.loja_list
+            filtro["vendedor__equipe_venda__loja__in"] = self.loja_list
         if self.equipe_venda_list:
-            filtro['vendedor__equipe_venda__in'] = self.equipe_venda_list
+            filtro["vendedor__equipe_venda__in"] = self.equipe_venda_list
         if self.produto_list:
-            filtro['produto__in'] = self.produto_list
+            filtro["produto__in"] = self.produto_list
         return filtro
 
     def run(self) -> dict:
         start_pandas_transform = timezone.now()
         self.extract_transform_dataset()
         finish_pandas_transform = timezone.now()
-        self.log['duration_transform'] = (
-            finish_pandas_transform -
-            start_pandas_transform).total_seconds()
+        self.log["duration_transform"] = (
+            finish_pandas_transform - start_pandas_transform
+        ).total_seconds()
         self.load()
         return self.log
 
     def extract_transform_dataset(self) -> None:
         self.dataset = (
-            self._venda_dataset
-            .merge(
-                self._imposto_dataset,
-                how='left', on=['loja_id'])
+            self._venda_dataset.merge(
+                self._imposto_dataset, how="left", on=["loja_id"]
+            )
             .assign(
-                valor_impostos=lambda d_:
-                    (d_['valor_receita_bruta'] * (d_['percent_imposto']/100))
-                    .round(2),
-                valor_comissao=lambda d_:
-                    ((d_['valor_receita_bruta']-d_['valor_impostos']) *
-                     (d_['percent_comissao']/100))
-                    .round(2),
+                valor_impostos=lambda d_: (
+                    d_["valor_receita_bruta"] * (d_["percent_imposto"] / 100)
+                ).round(2),
+                valor_comissao=lambda d_: (
+                    (d_["valor_receita_bruta"] - d_["valor_impostos"])
+                    * (d_["percent_comissao"] / 100)
+                ).round(2),
                 valor_receita_liquida=lambda d_: (
-                    d_['valor_receita_bruta'] -
-                    d_['valor_comissao'] -
-                    d_['valor_impostos'] -
-                    d_['valor_custo']
-                    )
-                .round(2)
+                    d_["valor_receita_bruta"]
+                    - d_["valor_comissao"]
+                    - d_["valor_impostos"]
+                    - d_["valor_custo"]
+                ).round(2),
             )
             .pipe(self._reshape_data)
             .assign(
                 tipo_id=lambda d_: (
-                    d_['receita_tipo'].map(
+                    d_["receita_tipo"].map(
                         {
-                            f'valor_{obj.codigo}': obj.id
-                            for obj in
-                            self.get_tipo_queryset()
+                            f"valor_{obj.codigo}": obj.id
+                            for obj in self.get_tipo_queryset()
                         }
                     )
                 )
             )
-            .groupby([
-                'data', 'produto_id',
-                'vendedor_id',
-                'tipo_id',
-            ], as_index=False)
-            .agg({'valor': 'sum'})
-            .astype({
-                'data': 'datetime64[D]',
-                'produto_id': 'int',
-                'vendedor_id': 'int',
-                'tipo_id': 'int',
-                'valor': 'float'
-            })
+            .groupby(
+                [
+                    "data",
+                    "produto_id",
+                    "vendedor_id",
+                    "cliente_id",
+                    "tipo_id",
+                ],
+                as_index=False,
+            )
+            .agg({"valor": "sum"})
+            .astype(
+                {
+                    "data": "datetime64[D]",
+                    "cliente_id": "int",
+                    "produto_id": "int",
+                    "vendedor_id": "int",
+                    "tipo_id": "int",
+                    "valor": "float",
+                }
+            )
         )
 
     @property
     def _venda_dataset(self) -> pd.DataFrame:
         """Retorna um dataset com os dados de [Venda]"""
         field_map = {
-            'data': 'data',
-            'produto_id': 'produto_id',
-            'vendedor_id': 'vendedor_id',
-            'valor_receita_bruta': 'valor_receita_bruta',
-            'vendedor__equipe_venda__percent_comissao': 'percent_comissao',
-            'vendedor__equipe_venda__loja_id': 'loja_id'
+            "data": "data",
+            "produto_id": "produto_id",
+            "vendedor_id": "vendedor_id",
+            "valor_receita_bruta": "valor_receita_bruta",
+            "vendedor__equipe_venda__percent_comissao": "percent_comissao",
+            "vendedor__equipe_venda__loja_id": "loja_id",
+            "cliente_id": "cliente_id",
         }
         qs = (
             self.get_venda_queryset()
             .values(*field_map)
-            .annotate(valor_custo=Sum('produto__custo'))
+            .annotate(valor_custo=Sum("produto__custo"))
         )
         return (
             pd.DataFrame(
-                data=qs,
-                columns=list(field_map.keys())+['valor_custo'])
+                data=qs, columns=list(field_map.keys()) + ["valor_custo"]
+            )
             .rename(columns=field_map)
             .astype(
                 dtype={
-                    'data': 'datetime64[ns]',
-                    'produto_id': 'int64',
-                    'vendedor_id': 'int64',
-                    'loja_id': 'int64',
-                    'valor_receita_bruta': 'float64',
-                    'valor_custo': 'float64',
-                    'percent_comissao': 'float64'
-                })
+                    "data": "datetime64[ns]",
+                    "produto_id": "int64",
+                    "vendedor_id": "int64",
+                    "loja_id": "int64",
+                    "valor_receita_bruta": "float64",
+                    "valor_custo": "float64",
+                    "percent_comissao": "float64",
+                }
+            )
             .round(2)
         )
 
@@ -150,28 +155,27 @@ class LoadConsolidacaoPandas(Pipeline):
         """Retorna um dataset com os dados de [Imposto]"""
         qs = (
             self.get_imposto_queryset()
-            .values('loja_id')
-            .annotate(percent_imposto=Sum('percent'))
+            .values("loja_id")
+            .annotate(percent_imposto=Sum("percent"))
         )
-        return (
-            pd.DataFrame(data=qs, columns=['loja_id', 'percent_imposto'])
-            .astype(
-                dtype={
-                    'loja_id': 'int64',
-                    'percent_imposto': 'float64'
-                })
-        )
+        return pd.DataFrame(
+            data=qs, columns=["loja_id", "percent_imposto"]
+        ).astype(dtype={"loja_id": "int64", "percent_imposto": "float64"})
 
     def _reshape_data(self, df: pd.DataFrame) -> pd.DataFrame:
         """Transforma todas as colunas de valores em duas, uma de tipo e outra de valor."""
         return df.melt(
             id_vars=[
-                'data', 'produto_id',
-                'vendedor_id', 'loja_id'
+                "data",
+                "cliente_id",
+                "produto_id",
+                "vendedor_id",
+                "loja_id",
             ],
-            value_vars=[col for col in df.columns if col.startswith('valor_')],
-            var_name='receita_tipo',
-            value_name='valor')
+            value_vars=[col for col in df.columns if col.startswith("valor_")],
+            var_name="receita_tipo",
+            value_name="valor",
+        )
 
     @transaction.atomic
     def load(self) -> None:
@@ -179,37 +183,27 @@ class LoadConsolidacaoPandas(Pipeline):
         self._save()
 
     def _delete(self):
-        n_deleted, __ = (
-            Consolidacao
-            .objects
-            .filter(**self._filtro)
-            .delete()
-        )
-        self.log['n_deleted'] = n_deleted
+        n_deleted, __ = Consolidacao.objects.filter(**self._filtro).delete()
+        self.log["n_deleted"] = n_deleted
 
     def _save(self):
         if self.dataset.empty:
             return
 
         objs = [
-            Consolidacao(**vals)
-            for vals in
-            self.dataset.to_dict('records')]
-        bulk = (
-            Consolidacao
-            .objects
-            .bulk_create(
-                objs=objs,
-                batch_size=1000))
-        self.log['n_inserted'] = len(bulk)
+            Consolidacao(**vals) for vals in self.dataset.to_dict("records")
+        ]
+        bulk = Consolidacao.objects.bulk_create(objs=objs, batch_size=1000)
+        self.log["n_inserted"] = len(bulk)
 
 
 @shared_task(
-    name='receita.load_consolidacao_pandas',
+    name="receita.load_consolidacao_pandas",
     bind=True,
     autoretry_for=(Exception,),
     retry_backoff=5,
-    retry_kwargs={'max_retries': 3})
+    retry_kwargs={"max_retries": 3},
+)
 def load_consolidacao_pandas_async(self, filtros: dict) -> dict:
     with LoadConsolidacaoPandas(**filtros) as task:
         log = task.run()
